ef92633c105a9954812f66a9070cbeb4bb2baffc
Dante-ELeuterio <danteeleuterio00@gmail.com>
2022-08-03 22:39:15 -0300

    CD E MKDIR 99%

--- BufferFunctions.c
+++ BufferFunctions.c
@@ -7,50 +7,68 @@ void imprime_buffer(header *head){
       printf("\t|tamanho :%d\n ",head->tamanho);
       printf("\t|sequencia :%d\n ",head->sequencia);
       printf("\t|tipo :%d\n",head->tipo);
+      printf("\t|dados: ");
+      for (int i = 0; i < head->tamanho; i++) {
+        unsigned char d = (((unsigned char*)(head)) + sizeof(header))[i];
+        if (d < 0x20) {
+            printf("[%d]", d);
+        } else {
+        printf("'%c'", d);
+        }
+      }
+      printf("\n");
 }
-void constroi_buffer(int soquete,int sequencia,unsigned char input[],int tipo){
+void constroi_buffer(int soquete,int sequencia,unsigned char input[],int tipo,int tamanho){
     unsigned char *sendbuff;
     sendbuff= (unsigned char*)malloc(BYTES);
     memset(sendbuff,0,BYTES);
     header *head = (header *)(sendbuff);
     head->mi=126;
-    head->tamanho=strlen(input);
+    head->tamanho=tamanho;
     head->sequencia=sequencia;
     head->tipo=tipo;
     int header_len=sizeof(header);
     int paridade=0;
-    for (int i = header_len; i < BYTES-1; i++)
+    for (int i = header_len; i < tamanho+header_len; i++)
     {
         sendbuff[i] = input[i-header_len];
         paridade^=sendbuff[i];
     }
     sendbuff[BYTES-1]=paridade;
+    printf("enviando\n");
     imprime_buffer(head);
     sendto(soquete,sendbuff,BYTES,0,NULL,0);
     free(sendbuff);
 }
 
 int DesmontaBuffer(unsigned char buffer[],unsigned char dados[],int *tipo,int *last_seq,int *seq_rec){
+    *seq_rec = DEFAULT;
+    int msg_esperada=0;
+    if(*last_seq!=15){
+        msg_esperada=*last_seq+1;
+    }
     header *head = (header *)(buffer);
     int paridade=0;
     unsigned char *data = (buffer + sizeof(header));
     if(head->sequencia==*last_seq){
         *tipo=head->tipo;
         *seq_rec=head->sequencia;
-        return 1;
+        return head->tamanho;
     }
     for (int i = 0; i < head->tamanho; i++){
         paridade^=data[i];
     }
-    if(head->sequencia!=*last_seq+1 || paridade!=buffer[BYTES-1]){
+    if(head->sequencia!=msg_esperada || paridade!=buffer[BYTES-1]){
+        printf("%d  %d  %d  %d\n",head->sequencia, msg_esperada, paridade, buffer[BYTES-1]);
         *tipo=NACK;
-        return 0;
+        return DEFAULT;
     }
     *last_seq=head->sequencia;
     *tipo=head->tipo;
-    strncpy(dados,data,63);
+    memcpy(dados,data,63);
+    printf("recebendo\n");
     imprime_buffer(head);
-    return 1;
+    return head->tamanho;
 }
 
 
--- servidor.c
+++ servidor.c
@@ -1,19 +1,20 @@
 #include "header.h"
 #include "BufferFunctions.h"
 
+//Flags de controle de sequência
 int last_seq;
-int sequencia_recebida;
+int aux;
 int sequencia;
-
-
+int tamanho;
+//Trata erro de recebimento e envia
 void falha(int soquete,unsigned char data[]){
   limpa_string(data,63);
   sequencia++;
-  if(last_seq==15)
-    data[0]=0;
+  if(last_seq==15)   //Confere se chegou no final da sequencia
+    data[0]=0;      
   else
     data[0]=last_seq+1;
-  constroi_buffer(soquete,sequencia,data,NACK);
+  constroi_buffer(soquete,sequencia,data,NACK,1);
 }
 
 void trata_tipo(int tipo,unsigned char data[],int soquete){
@@ -29,24 +30,75 @@ void trata_tipo(int tipo,unsigned char data[],int soquete){
       if(data[0]==3)
         system("ls -a -l > dados.txt");
     system("cat dados.txt");
+    system("rm -f dados.txt");
     break;
   case CD:
-    if(sequencia_recebida==last_seq){
+    if(aux==last_seq){
       limpa_string(data,63);
       sequencia++;
-      constroi_buffer(soquete,sequencia,data,OK);
+      constroi_buffer(soquete,sequencia,data,OK,0);
     }
     else{
-      chdir(data);
-      limpa_string(data,63);
       sequencia++;
-      constroi_buffer(soquete,sequencia,data,OK);
+      data[tamanho] = '\0';
+      if(!chdir(data)){
+        // TO DO: Arrumar
+        constroi_buffer(soquete,sequencia,data,OK,0);
+      }
+      else{
+        switch (errno){
+        case EACCES:
+        case EFAULT:
+          data[0]='B';
+          break;
+        case ENOTDIR:
+        case ENOENT:
+          data[0]='A';
+          break;
+        default:
+          data[0]='Z';
+          break;
+        }
+        constroi_buffer(soquete,sequencia,data,ERRO,1);
+      }
     }
     if(sequencia==16)
       sequencia=0;
     break;
   case MKDIR:
-    mkdir(data,0700);
+      if(aux==last_seq){
+      limpa_string(data,63);
+      sequencia++;
+      constroi_buffer(soquete,sequencia,data,OK,0);
+    }
+    else{
+      sequencia++;
+      data[tamanho] = '\0';
+      if(!mkdir(data,0700)){
+        // TO DO: Arrumar
+        constroi_buffer(soquete,sequencia,data,OK,0);
+      }
+      else{
+        switch (errno){
+        case EACCES:
+        case EFAULT:
+          data[0]='B';
+          break;
+        case EEXIST:
+          data[0]='C';
+          break;
+        case ENOSPC:
+          data[0]='E';
+          break; 
+        default:
+          data[0]='Z';
+          break;
+        }
+        constroi_buffer(soquete,sequencia,data,ERRO,1);
+      }
+    }
+    if(sequencia==16)
+      sequencia=0;
     break;
   case GET:
 
@@ -74,31 +126,31 @@ void trata_tipo(int tipo,unsigned char data[],int soquete){
 int main(){
   int buflen;
   int sock_r;
-  int tipo=-1;
+  int tipo=DEFAULT;
   sock_r = ConexaoRawSocket("enp7s0f0");
   unsigned char *buffer = (unsigned char *) malloc(BYTES); //to receive data
   memset(buffer,0,BYTES);
   unsigned char data[63];
-  limpa_string(buffer,67);
+  limpa_string(buffer,BYTES);
   limpa_string(data,63);
   sequencia=-1;
-  last_seq=10;
-  sequencia_recebida=10;
+  last_seq=15;
   while(1)
   {  
-    limpa_string(buffer,67);
+    limpa_string(buffer,BYTES);
     limpa_string(data,63);
     buflen=recvfrom(sock_r,buffer,BYTES,0,NULL,0);
     if(buflen<0){
       printf("error in reading recvfrom function\n");
       return -1;
     }
-  if(buffer[0]==126){
-    if(DesmontaBuffer(buffer,data,&tipo,&last_seq,&sequencia_recebida))
-      trata_tipo(tipo,data,sock_r);
-    else
-      falha(sock_r,data);
-  }
+    if(buffer[0]==126){
+      tamanho=DesmontaBuffer(buffer,data,&tipo,&last_seq,&aux);
+      if(tamanho!=DEFAULT)
+        trata_tipo(tipo,data,sock_r);
+      else
+        falha(sock_r,data);
+    }
   }
   return 0;
 }
